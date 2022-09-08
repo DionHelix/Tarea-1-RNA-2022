93cd7ce5fa90796db861ae90a33ba8bd72ded252
DionHelix <dionhelix@gmail.com>
2022-09-08 18:15:11 -0500

    Se ha corrido el código y desde la primer época obtuvo 8196/10000, y en la última se obtuvo 9510/10000. Este código corrió con 30 épocas, 784 neuronas de entrada y 10 de salida, un Learning Rate de 3.0, entrenando con el optimizador SGD

--- Tarea1.py
+++ Tarea1.py
@@ -5,4 +5,12 @@ import network3
 import pickle
 import adam_optim 
 from adam_optim import AdamOptim
-from network3 import Network, FullyConnectedLayer, SoftmaxLayer
+from network3 import Network, FullyConnectedLayer, SoftmaxLayer 
+
+training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
+
+training_data = list(training_data)
+test_data = list(test_data)
+
+net=network.Network([784,30,10])
+net.SGD( training_data, 30, 10, 3.0, test_data=test_data)
