b46f7e924e5e6a335bd2ba4c10901023f9bb42ea
DionHelix <dionhelix@gmail.com>
2022-09-08 22:37:09 -0500

    Se corrió el código en 50 épocas, vemos que respecto a los intentos anteriores la eficiencia bajó a 8997/10000

--- Tarea1.py
+++ Tarea1.py
@@ -13,5 +13,5 @@ training_data = list(training_data)
 test_data = list(test_data)
 
 net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-net.SGD(training_data, 30, 10, 3.0, lmbda = 5.0,evaluation_data=validation_data,
+net.SGD(training_data, 50, 10, 3.0, lmbda = 5.0,evaluation_data=validation_data,
     monitor_evaluation_accuracy=True)
