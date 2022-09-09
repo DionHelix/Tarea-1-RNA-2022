2d40d29f0e04b18047d7c1aa57133ba5fd71c392
DionHelix <dionhelix@gmail.com>
2022-09-08 22:27:53 -0500

    Agregamos funcion de costo CrossEntropy en 30 épocas

--- Tarea1.py
+++ Tarea1.py
@@ -12,5 +12,6 @@ training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
 
-net=network.Network([784,30,10])
-net.SGD( training_data, 50, 10, 3.0, test_data=test_data)
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net.SGD(training_data, 30, 10, 3.0, lmbda = 5.0,evaluation_data=validation_data,
+    monitor_evaluation_accuracy=True)
