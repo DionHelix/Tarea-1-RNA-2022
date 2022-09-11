f31a07742f07f88c7a377cf5b61253f27cc9f6b0
DionHelix <dionhelix@gmail.com>
2022-09-11 00:54:34 -0500

    Se ha tratado de implementar directamente el código de adam_optim.py dentro de una copia de network.py llamada network4.py, donde hemos modificado el código interno de SGD para implementar las matemáticas detras de ADAM optimizer. Hasta ahora no se ha logrado hacer que al correr el código, la terminal no arroje error de sintaxis. Antes de correr las instrucciones en Tarea1.py, primero se ha utilizado el archivo Pruebas de Sintaxis.py para probar que no haya errores de escritura antes de hacer las pruebas reales de eficiencia

--- Pruebas de Sintaxis.py
+++ Pruebas de Sintaxis.py
@@ -2,6 +2,7 @@ import mnist_loader
 import network
 import network2
 import network3
+import network4
 import pickle
 # import theano
 # import theano.tensor as T
@@ -40,4 +41,10 @@ from network3 import Network, ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 #     SoftmaxLayer(n_in=30, n_out=10)], mini_batch_size)
 # net.SGD(training_data, 5, mini_batch_size, 0.1, validation_data, test_data)
 
+training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 
+training_data = list(training_data)
+test_data = list(test_data)
+
+net=network4.Network([784,30,10])
+net.ADAM( training_data, 30, 10, 3.0, test_data=test_data)
