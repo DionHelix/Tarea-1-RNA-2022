307edb3803b499c8dc34c5a7a69cce514c84ffa9
DionHelix <dionhelix@gmail.com>
2022-09-18 15:50:54 -0500

    Se realizarón parte de los cambios propuestos, pero ahora surge un nuevo error. Aún falta definir m_dw_corr, m_db_corr con su naturaleza matricial, pero no se realizará hasta corregir o al menos entender que está pasando con los cambios más recientes.

--- network4.py
+++ network4.py
@@ -71,13 +71,13 @@ class Network(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta, beta1=0.9, beta2=0.999, epsilon=1e-8)
+                self.update_mini_batch(mini_batch, eta, beta1=0.9, beta2=0.999, epsilon=1e-8, t, w, b, dw, db)
             if test_data:
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
             else:
                 print("Epoch {} complete".format(j))
 
-    def update_mini_batch(self, mini_batch, eta, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    def update_mini_batch(self, mini_batch, eta, beta1=0.9, beta2=0.999, epsilon=1e-8, t, w, b, dw, db):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
@@ -89,35 +89,35 @@ class Network(object):
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
             
-        self.m_dw, self.v_dw = 0, 0
-        self.m_db, self.v_db = 0, 0
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
-        self.eta = eta
-        def update(self, t, w, b, dw, db):
-            ## dw, db are from current minibatch
-            ## momentum beta 1
-            # *** weights *** #
-            self.m_dw = self.beta1*self.m_dw + (1-self.beta1)*dw
-            # *** biases *** #
-            self.m_db = self.beta1*self.m_db + (1-self.beta1)*db
+    self.m_dw, self.v_dw = 0, 0
+    self.m_db, self.v_db = 0, 0
+    self.beta1 = beta1
+    self.beta2 = beta2
+    self.epsilon = epsilon
+    self.eta = eta
+
+    ## dw, db are from current minibatch
+    ## momentum beta 1
+    # *** weights *** #
+    self.m_dw = self.beta1*self.m_dw + (1-self.beta1)*dw
+    # *** biases *** #
+    self.m_db = self.beta1*self.m_db + (1-self.beta1)*db
+
+    ## rms beta 2
+    # *** weights *** #
+    self.v_dw = self.beta2*self.v_dw + (1-self.beta2)*(dw**2)
+    # *** biases *** #
+    self.v_db = self.beta2*self.v_db + (1-self.beta2)*(db)
+
+    ## bias correction
+    m_dw_corr = self.m_dw/(1-self.beta1**t)
+    m_db_corr = self.m_db/(1-self.beta1**t)
+    v_dw_corr = self.v_dw/(1-self.beta2**t)
+    v_db_corr = self.v_db/(1-self.beta2**t)
     
-            ## rms beta 2
-            # *** weights *** #
-            self.v_dw = self.beta2*self.v_dw + (1-self.beta2)*(dw**2)
-            # *** biases *** #
-            self.v_db = self.beta2*self.v_db + (1-self.beta2)*(db)
-    
-            ## bias correction
-            m_dw_corr = self.m_dw/(1-self.beta1**t)
-            m_db_corr = self.m_db/(1-self.beta1**t)
-            v_dw_corr = self.v_dw/(1-self.beta2**t)
-            v_db_corr = self.v_db/(1-self.beta2**t)
-            
-            self.weights = [w - self.eta*(m_dw_corr/(np.sqrt(v_dw_corr)+self.epsilon)) for w in zip(self.weights, nabla_w)]
-            self.biases = [b - self.eta*(m_db_corr/(np.sqrt(v_db_corr)+self.epsilon)) for b in zip(self.biases, nabla_b)]
-            return self.weights, self.biases            
+    self.weights = [w - self.eta*(m_dw_corr/(np.sqrt(v_dw_corr)+self.epsilon)) for w in zip(self.weights, nabla_w)]
+    self.biases = [b - self.eta*(m_db_corr/(np.sqrt(v_db_corr)+self.epsilon)) for b in zip(self.biases, nabla_b)]
+    return self.weights, self.biases        
 
         
     def backprop(self, x, y):
